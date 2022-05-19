c89d752d64f1c43cce8eefd4cb3627ce53fe50b7
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-05-19 14:35:08 +0100

    added widget constructor

--- Spacy/cfg.py
+++ Spacy/cfg.py
@@ -1,5 +1,7 @@
+import tkinter as tk
 from configparser import ConfigParser
 from appdirs import AppDirs
+from distutils.util import strtobool
 from os import path
 
 from constants import OUTPUT_DIR
@@ -25,3 +27,16 @@ class ConfigManager(ConfigParser):
         }
         with open(self.fp, 'w') as file: 
             self.write(file)
+
+    def create_settings_vars(self) -> list:
+        """returns list of tk vars created from the configuration"""
+        variables = []
+        for k, val in self['settings'].items():
+            try:
+                val = strtobool(val)
+                var = tk.BooleanVar()
+            except ValueError:
+                var = tk.StringVar()
+            var.set(val)
+            variables.append((k, var))
+        return variables
