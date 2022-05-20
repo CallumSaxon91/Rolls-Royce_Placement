c68ec8f45e89450a613b3bab37b3a17473e099e0
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-05-20 14:42:39 +0100

    added entities to defaults

--- Spacy/cfg.py
+++ Spacy/cfg.py
@@ -7,24 +7,71 @@ from os import path
 from constants import OUTPUT_DIR
 
 
+defaults = {
+    'settings': {
+        'auto_save': 'no',
+        'auto_save_path': str(OUTPUT_DIR),
+        'quick_search': 'no'
+    },
+    'entities': {
+        'PEOPLE': 'People, including fictional characters.',
+        'NORP': 'Nationalities or religious or political groups.',
+        'FAC': 'Buildings, airports, highways, bridges, etc.',
+        'ORG': 'Companies, agencies, institutions, etc.',
+        'LOC': 'Non-GPE locations, mountain ranges, bodies of water.',
+        'PRODUCT': 'Objects, vehicles, foods, etc. (Not services.)',
+        'EVENT': 'Named hurricanes, battles, wars, sports events, etc.',
+        'WORK_OF_ART': 'Titles of books, songs, etc.',
+        'LAW': 'Named documents made into laws.',
+        'LANGUAGE': 'Any named language.',
+        'DATE': 'Absolute or relative dates or periods.',
+        'TIME': 'Times smaller than a day.',
+        'PERCENT': 'Percentage',
+        'MONEY': 'Monetary values, including unit.',
+        'QUANTITY': 'Measurements, as of weight or distance.',
+        'ORDINAL': '“first”, “second”, etc.',
+        'CARDINAL': 'Numerals that do not fall under another type.'
+    },
+    'POS_tags': {  # universal part of speech categories
+        # open
+        'ADJ': 'Adjective',
+        'ADV': 'Adverb',
+        'INTJ': 'Interjection',
+        'NOUN': 'Noun',
+        'PROPN': 'Proper Noun',
+        'VERB': 'Verb',
+        # closed
+        'ADP': 'Adposition',
+        'AUX': 'Auxiliary',
+        'CCONJ': 'Coordinating Conjunction',
+        'DET': 'Determiner',
+        'NUM': 'Numeral',
+        'PART': 'Particle',
+        'PRON': 'Pronoun',
+        'SCONJ': 'Subordinating Cojunction',
+        # other
+        'PUNCT': 'Punctuation',
+        'SYM': 'Symbol',
+        'X': 'Other'
+    }
+}
+
+
 class ConfigManager(ConfigParser):
     """Configuration manager for spacy research project"""
     def __init__(self, dirs:AppDirs):
         super().__init__()
         self.dirs = dirs.user_config_dir
         self.fp = f'{self.dirs}/config.ini'
-        self.validate()
+        self.validate(True)
         self.read(self.fp)
 
     def validate(self, force_restore:bool=False):
         """create config file if it doesnt exist"""
         if path.exists(self.fp) and not force_restore:
             return
-        self['settings'] = {
-            'auto_save': 'no',
-            'auto_save_path': OUTPUT_DIR,
-            'quick_search': 'yes'
-        }
+        for section, options in defaults.items():
+            self[section] = options
         with open(self.fp, 'w') as file: 
             self.write(file)
 
