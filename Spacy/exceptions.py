42d72de39947db7f788a0297e459011479050de3
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-06-01 11:59:49 +0100

    Added Pipeline exception + cleaned ImageNotFound

--- Spacy/exceptions.py
+++ Spacy/exceptions.py
@@ -1,5 +1,19 @@
 
 
 class ImageNotFound(Exception):
-    """Could not find an image file at this fp"""
-    pass
+    """Could not find an image file at this location"""
+    def __init__(self, fp:str):
+        self.fp = fp
+        self.message = f"Could not find image file at location: {fp}"
+        super().__init__(self.message)
+
+
+class PipelineNotLoaded(Exception):
+    """
+        Tried to run process that depends on a pipeline before a
+        pipeline was loaded
+    """
+    def __str__(self) -> str:
+        return "Cannot complete this process because a pipeline " \
+               "has not been loaded"
+
