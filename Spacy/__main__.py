ad56161c2c9cb202819fbb4504197f2accb37a27
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-06-07 13:44:17 +0100

    Restarting now revalidates dirs and re-setups logs

--- Spacy/__main__.py
+++ Spacy/__main__.py
@@ -14,19 +14,20 @@ def restart(root):
     main()
 
 def main():
-    Root(
-        name=APP_NAME,
-        dirs=directories,
-        restart_func=restart
-    ).start()
-
-if __name__ == '__main__':
     log.info('Starting application')
     # Validate app directories exist and setup logging
     directories = AppDirs(APP_NAME)
     validate_dirs(directories)
     setup_logs(directories)
     # Create and start GUI
-    main()
+    Root(
+        name=APP_NAME,
+        dirs=directories,
+        restart_func=restart
+    ).start()
     # This line will only be read if the GUI has been closed properly
     log.info('Exited application successfully')
+
+if __name__ == '__main__':
+    main()
+    
